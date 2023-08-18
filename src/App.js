73c01dbb92210dde5ea0c7ed8ae2f8ed77121535
kkadd <kadsiriwan@gmail.com>
2023-08-18 14:54:40 +0700

    banner & calculator2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Banner from "./banner";
 import { Header, Content, Footer } from "./func-components";
 import Calender from "./class-components";
+import { Calculator2 } from "./calculator";
 
 function App(){
   return (
@@ -8,6 +10,8 @@ function App(){
       <Header/>
       <p><center><Calender/></center></p>
       <Content/>
+      <Calculator2/>
+      <Banner/>
       <Footer/>
     </>
   )

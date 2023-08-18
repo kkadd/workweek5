fee2500e7d95f96c599262f799fb1358ba7d6baf
kkadd <kadsiriwan@gmail.com>
2023-08-18 15:27:03 +0700

    add file event-data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import Banner from "./banner";
-import { Header, Content, Footer } from "./func-components";
-import Calender from "./class-components";
-import { Calculator2 } from "./calculator";
+import { Header, Footer } from "./func-components";
+import { EventData2 } from "./event-data";
 
 function App(){
   return (
     <>
       <Header/>
-      <p><center><Calender/></center></p>
-      <Content/>
-      <Calculator2/>
-      <Banner/>
+      <EventData2/>
       <Footer/>
     </>
   )

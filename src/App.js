578e1a492bf30ced3d3c6125ee67f1909e519159
kkadd <kadsiriwan@gmail.com>
2023-10-06 22:30:56 +0700

    react hook_formValidation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import FormCheck from './form-check'
 import FormText from './form-text'
+import FormValidation from './form-validation'
 export default function App() {
   
   return (
-    <FormText/>
+    <FormValidation/>
   )
 }
